b12b5a087c22302653130e1e911bc8c1fff6f7a3
Spencer B <spencerbillings2000@gmail.com>
2018-12-14 08:39:18 -0700

    Added the splitViewController method and added the InternetDetailViewController class

---
+++ NetworkProject/network.controller/InternetDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InternetDetailViewController.swift
+//  NetworkProject
+//
+//  Created by Billings, Spencer on 12/14/18.
+//  Copyright © 2018 Billings, Spencer. All rights reserved.
+//
+
+import UIKit
+
+public class InternetDetailViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
