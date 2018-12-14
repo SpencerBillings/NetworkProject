fd4e65484bc1646ddb5fe41e0a19397b10da9068
Spencer B <spencerbillings2000@gmail.com>
2018-12-14 09:41:27 -0700

    Created the InternetMasterViewController class and deleted the ViewController class

--- NetworkProject/network.controller/InternetDetailViewController.swift
+++ NetworkProject/network.controller/InternetDetailViewController.swift
@@ -43,7 +43,7 @@ public class InternetDetailViewController: UIViewController
             {
                 if(detailAddress != nil)
                 {
-                    loadURL(webAddress detailAddress!)
+                    loadURL(webAddress: detailAddress!)
                 }
             }
             screenTitle?.text = detailTitle
---
+++ NetworkProject/network.controller/InternetMasterViewController.swift
@@ -0,0 +1,139 @@
+//
+//  InternetMasterViewController.swift
+//  NetworkProject
+//
+//  Created by Billings, Spencer on 12/14/18.
+//  Copyright © 2018 Billings, Spencer. All rights reserved.
+//
+
+import UIKit
+
+public class InternetMasterViewController: UITableViewController
+{
+    
+    private weak var internetDetail : InternetDetailViewController? = nil
+    private lazy var internetTopics : [String] = [String]()
+    private lazy var addresses : [String] = [String]()
+    
+    private func setupDetailContents() -> Void
+    {
+        internetTopics = [
+            "Internet Definitions 📖",
+            "Standard Search Engine 🔍",
+            "AP CSP 💾",
+            "Canyons District 🍎",
+            "CTEC 🍏",
+            "Social Media 🌐"
+        ]
+        
+        addresses = [
+            "https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html",
+            "https://www.google.com",
+            "https://apcentral.collegeboard.org/courses/ap-computer-science-principles?course=ap-computer-science-principles",
+            "http://www.canyonsdistrict.org/",
+            "https://ctec.canyonsdistrict.org/",
+            "https://www.instagram.com/spencerblessings/"
+        ]
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            internetDetail = currentControllers[0] as? InternetDetailViewController
+        }
+    }
+    
+    // This method only happens once with the split view
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+
+        setupDetailContents()
+        self.clearsSelectionOnViewWillAppear = false
+
+        if let split = splitViewController
+        {
+            let controllers = split.viewControllers
+            internetDetail = (controllers[controllers.count-1] as! UINavigationController).topViewController as? InternetDetailViewController
+        }
+    }
+
+    /**
+    This method will execute multiple times i.e. every time the menu (master) is displayed
+    */
+    public override func viewWillAppear(_ animated: Bool) -> Void
+    {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - Table view data source
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return addresses.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let rowText = internetTopics[indexPath.row]
+        cell.textLabel!.text = rowText
+        return cell
+    }
+
+    public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) -> Void
+    {
+        internetDetail?.detailAddress = addresses[indexPath.row]
+        internetDetail?.detailTitle = internetTopics[indexPath.row]
+        if(internetDetail != nil)
+        {
+            splitViewController?.showDetailViewController(internetDetail!, sender: nil)
+        }
+    
+    }
+    
+}
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
